105dba252a88222e2f1518934153e2064d30111a
Abner Silva <contato@altechno.com.br>
2024-09-09 16:51:59 -0300

    sample

--- api/index.go
+++ api/index.go
@@ -1,29 +1,27 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+var (
+	app *gin.Engine
+)
+
+func myRoute(r *gin.RouterGroup) {
+	r.GET("/admin", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	})
+}
+
+func init() {
+	app = gin.New()
+	r := app.Group("/api")
+	myRoute(r)
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	server := New()
-
-	server.get("/", func(context *Context) {
-		context.JSON(200, H{
-			"message": "hello from vercel",
-		})
-	})
-
-	server.get("/name", func(context *Context) {
-		name := context.Query("name")
-		if name == "" {
-			context.JSON(400, H{
-				"message": "name not found",
-			})
-		} else {
-			context.JSON(200, H{
-				"message": fmt.Sprintf("hello from %s", name),
-			})
-		}
-	})
+	app.ServeHTTP(w, r)
 }
